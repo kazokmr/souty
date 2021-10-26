592aa6faf0726c14a169ec78be4d9b75abd80924
Kazuhiko Okumura <engkokmr@gmail.com>
2021-10-27 07:33:49 +0900

    Cucumber Runnerの設定変更 - Cucumber7.0にあげると@Cucumberがdeprecatedになった事による対応 - junit-platform-suiteを依存関係に追加 - CucumberRunnerに@Suiteアノテーション等を追加（多分なくても大丈夫）

--- src/test/java/io/cucumber/RunCucumberTest.java
+++ src/test/java/io/cucumber/RunCucumberTest.java
@@ -1,7 +1,15 @@
 package io.cucumber;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("io/cucumber")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber")
 public class RunCucumberTest {
 }
